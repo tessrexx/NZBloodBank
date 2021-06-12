e8fac38480220d672d38db8db7a6f7601f69041a
TessRexx <270054867@yoobeestudent.ac.nz>
2021-06-13 02:08:55 +1200

    Readded updated Donor Login & Registration. Still need to solve cin.ignore issue

--- NZBloodBank/NZBloodBank.cpp
+++ NZBloodBank/NZBloodBank.cpp
@@ -12,34 +12,33 @@
 
 using namespace std;
 
-const int SIZE = 30; //Constant for Description Size
+const int SIZE = 31;
 
-struct Donor_Login
+struct Donor_Details //Includes Registration Details
 {
+    char first_name[SIZE];
+    char last_name[SIZE];
+    int date_of_birth;
+    char gender[SIZE]; 
+    char nationality[SIZE];
+    char ethnicity[SIZE];
+    char health_conditions[SIZE];
+    char blood_group[SIZE];
+    double contact_number;
+    char email_address[SIZE];
+    char address_line1[SIZE];
+    char address_line2[SIZE];
+    int last_donation;
+
     char username[SIZE];
     char password[SIZE];
 };
 
 //Structure containing information required to register a new donor
-struct New_Donor
-{
-    char first_name[SIZE];
-    char last_name[SIZE];
-    int date_of_birth;
-    char gender; // List M/F/O ??
-    char nationality[SIZE];
-    char ethnicity[SIZE];
-    char health_conditions[SIZE];
-    char blood_group = NULL;
-    double contact_number;
-    char email_address[SIZE];
-    char address_line1[SIZE];
-    char address_line2[SIZE];
-    int last_donation = 00 / 00 / 00;
-};
+
 
 //Structure containing information required to register a new recipient
-struct New_Recipient
+struct Recipient_Details
 {
     char name[SIZE];
     char email_address[SIZE];
@@ -50,9 +49,11 @@ struct New_Recipient
 };
 
 //Functions (some will require pointers at some point)
-void introduction_menu();
-void donor_login(fstream&);
-void donor_registration();
+void main_menu();
+void introduction_menu(fstream& login);
+void about_and_contact();
+void donor_login(fstream& login); 
+void donor_registration(fstream& login);
 void donor_menu();
 void book_donation();
 void update_information();
@@ -62,88 +63,55 @@ void recipient_menu();
 void admin_login();
 void admin_menu();
 void line();
+void border_line();
 
 int main()
 {
-    introduction_menu();
+    fstream login;
+
+    border_line();
+    printf("\x1B[93m\n\t\t\t\t  AOTEAROA BLOOD\033[0m\n");
+    border_line();
+
+    cout << "  Welcome to Aotearoa Blood, where we make blood donation easy." << endl;
+    cout << "  Use menu below to navigate through our system." << endl;
+    line();
+    introduction_menu(login);
 
     return 0;
 }
 
 //Function to Display Introduction & Menu
-void introduction_menu()
+void introduction_menu(fstream& login)
 {
     int option;
 
-    cout << endl << "\tAOTEAROA BLOOD";
-    line();
-
-    //Main Menu Display
-    cout << " Main Menu";
-    line();
-    cout << " 1. Information & Contact Us" << endl;
-    cout << " 2. Existing Donor" << endl;
-    cout << " 3. New Donor" << endl;
-    cout << " 4. Existing Recipient" << endl;
-    cout << " 5. New Recipient" << endl;
-    cout << " 6. Admin" << endl;
-    cout << " 7. Exit..." << endl;
-
-    cout << endl << " Enter your option: "; //User Option Input
-    cin >> option;
-    cin.ignore(); //Clears input buffer
-    cout << endl;
-
-    do
+    while (1)
     {
+        // show main menu first before switch cases options
+        main_menu();
+        cout << endl << " Enter your option: "; //User Option Input
+        cin >> option;
+        cin.ignore(); //Clears input buffer
+        //cout << endl;
+
         switch (option)
         {
 
-        case 1: // Information & Contact Us
+        case 1: // About & Contact Us
         {
-            system("cls"); //Clears console screen
-            cout << endl << "\tAOTEAROA BLOOD";
-
-            line();
-            cout << " About Aotearoa Blood";
-            line();
-            cout << " Aotearoa Blood was founded in 1998 under the New Zealand Public Health and Disability Act 2000" << endl;
-            cout << " and is an appointed entity under section 63 of the Human Tissue Act 2008," << endl;
-            cout << " with primary responsibility for blood and controlled human substances in New Zealand." << endl;
-            cout << " Prior to this, a variety of regional blood services operated out of hospitals." << endl;
-
-            line();
-            cout << " Contact us: ";
-            line();
-            cout << " Phone: 0800 000 000" << endl;
-            cout << " Email: aotearoa@blood.co.nz" << endl;
-            line();
-
-            cout << " Main Menu";
-            line();
-            cout << " 1. Information & Contact Us" << endl;
-            cout << " 2. Existing Donor" << endl;
-            cout << " 3. New Donor" << endl;
-            cout << " 4. Existing Recipient" << endl;
-            cout << " 5. New Recipient" << endl;
-            cout << " 6. Admin" << endl;
-            cout << " 7. Exit..." << endl;
-
-            cout << endl << " Enter your option: "; //User Option Input
-            cin >> option;
-            cin.ignore(); //Clears input buffer
-            cout << endl;
-
+            system("cls"); // clears screen
+            about_and_contact();
             break;
         }
         case 2: // Existing Donor
         {
-            donor_login();
+            donor_login(login);
             break;
         }
         case 3: // New Donor
         {
-            donor_registration();
+            donor_registration(login);
             break;
         }
         case 4: // Existing Recipient
@@ -158,7 +126,7 @@ void introduction_menu()
         }
         case 6: // Admin
         {
-            admin_login();
+            admin_menu();
             break;
         }
         default: exit(0); //Exit Console
@@ -166,51 +134,83 @@ void introduction_menu()
     } while (option < 7);
 }
 
-//Existing donor login (MAY COMBINE WITH DONOR MENU)
+// Function to display Main Menu 
+void main_menu()
+{
+    cout << " MAIN MENU\n";
+
+    cout << " 1. Information & Contact Us" << endl;
+    cout << " 2. Existing Donor" << endl;
+    cout << " 3. New Donor" << endl;
+    cout << " 4. Existing Recipient" << endl;
+    cout << " 5. New Recipient" << endl;
+    cout << " 6. Admin" << endl;
+    cout << " 7. Exit..." << endl;
+}
+
+// Function to Display info about the blood bank & contact details
+void about_and_contact()
+{
+    cout << endl;
+    border_line();
+    printf("\x1B[35m\n\t\t\t\t  ABOUT AOTEAROA BLOOD\033[0m\n\n");
+    //cout << "\t\t\t\t\tABOUT AOTEAROA BLOOD";
+    cout << "  Aotearoa Blood was founded in 1998 under the New Zealand Public Health and Disability Act 2000" << endl;
+    cout << "  and is an appointed entity under section 63 of the Human Tissue Act 2008," << endl;
+    cout << "  with primary responsibility for blood and controlled human substances in New Zealand." << endl;
+    cout << "  Prior to this, a variety of regional blood services operated out of hospitals." << endl;
+
+    cout << endl << "  CONTACT US " << endl;
+    cout << "  Phone: 0800 000 000" << endl;
+    cout << "  Email: aotearoa@blood.co.nz" << endl;
+    cout << endl << endl;
+    line();
+}
+
+//Existing donor login
 void donor_login(fstream& login)
 {
     system("cls"); //Clears console screen
     cout << endl << "\tAOTEAROA BLOOD";
     line();
 
-    // Calling options to read data
-    fstream login;
-    Donor_Login donor_record;
+    Donor_Details readRecord;
     bool flag = false;
-    char username[SIZE];
-    char password[SIZE];
+    char name[SIZE];
+    char pass[SIZE];
 
     // Open the file for reading
-    login.open("donor_record.txt", ios::in | ios::binary);
+    login.open("donor.dat", ios::in | ios::binary);
 
     if (!login)
     {
-        cout << "Error in opening the file . ";
+        cout << "Error in opening the file... ";
     }
     else
     {
+        //User Input
         cout << endl << "Enter Username: ";
-        cin.getline(username, SIZE);
-        cout << "Enter Password:";
-        cin.getline(password, SIZE);
+        cin.getline(name, SIZE);
+        cout << "Enter Password: ";
+        cin.getline(pass, SIZE);
 
-        // Now read, search, and display the records
-        login.read(reinterpret_cast<char*>(&donor_record), sizeof(donor_record));
+        // Read & search for matching data
+        login.read(reinterpret_cast<char*>(&readRecord), sizeof(readRecord));
 
         while (!login.eof())
         {
-            if ((strcmp(username, donor_record.username) == 0) && (strcmp(password, donor_record.password) == 0))
+            if ((strcmp(name, readRecord.username) == 0) && (strcmp(pass, readRecord.password) == 0))
             {
-                cout << "Login Successful." << endl;
+                donor_menu();
                 flag = true;
             }
-            login.read(reinterpret_cast<char*>(&donor_record), sizeof(donor_record));
+            login.read(reinterpret_cast<char*>(&readRecord), sizeof(readRecord));
         }
     }
-
+    // Show if now matching details found
     if (flag == false)
     {
-        cout << "Login Unsuccessful." << endl;
+        cout << endl << "Login not successful. Please try again." << endl;
     }
 
     login.close();
@@ -218,13 +218,86 @@ void donor_login(fstream& login)
     line();
 }
 
-//New donor registration 
-void donor_registration()
+//Function to obtain New Donor Registration info
+void donor_registration(fstream& login)
 {
+    // Variables needed to write the file
+    Donor_Details record;
+    // Open file
+    login.open("donor.dat", ios::out | ios::app | ios::binary);
 
+    if (!login)
+    {
+        cout << "Error opening file.";
+    }
+    else
+    {
+        // User input & writing in data file
+        cout << "  First Name: ";
+        cin.getline(record.first_name, SIZE);
+
+        cout << "  Last Name: ";
+        cin.getline(record.last_name, SIZE);
+
+        cout << "  Date of Birth: ";
+        cin >> record.date_of_birth;
+
+        cout << "  Gender: ";
+        cin.getline(record.gender, SIZE);
+        cin.ignore();
+
+        cout << "  Nationality: ";
+        cin.getline(record.nationality, SIZE);
+        cin.ignore();
+
+        cout << "  Ethnicity: ";
+        cin.getline(record.ethnicity, SIZE);
+        cin.ignore();
+
+        cout << "  Health Conditions: ";
+        cin.getline(record.health_conditions, SIZE);
+        cin.ignore();
+
+        cout << "  Blood Group: ";
+        cin.getline(record.blood_group, SIZE);
+        cin.ignore();
+
+        cout << "  Contact Number: ";
+        cin >> record.contact_number;
+
+        cout << "  Email Address: ";
+        cin.getline(record.email_address, 40);
+        cin.ignore();
+
+        cout << "  Street Address: ";
+        cin.getline(record.address_line1, SIZE);
+        cin.ignore();
+
+        cout << "  Suburb/City: ";
+        cin.getline(record.address_line2, SIZE);
+        cin.ignore();
+
+        cout << "  Date of Last Donation (Optional): ";
+        cin >> record.last_donation;
+
+        // User chooses their udername & password for future use 
+        cout << endl << endl << "  Username: ";
+        cin.getline(record.username, SIZE);
+        cin.ignore();
+
+        cout << "  Password: ";
+        cin.getline(record.password, SIZE);
+        cin.ignore();
+
+        cout << "Now writing record...." << endl;
+
+        login.write(reinterpret_cast<char*>(&record), sizeof(record));
+    }
+    // Close the file
+    login.close();
 }
 
-//Function to display Donor Menu (MAY COMBINE WITH DONOR lOGIN)
+//Function to display Donor Menu 
 void donor_menu()
 {
     system("cls"); //Clears console screen
@@ -239,6 +312,7 @@ void donor_menu()
     do
     {
         //Main Menu Display
+        line();
         cout << " Donor Menu";
         line();
         cout << " 1. Procedure to Donate Blood" << endl;
@@ -286,12 +360,12 @@ void donor_menu()
             cout << " Benefits of Blood Donation";
             line();
 
-            cout << " • One blood donation can save up to 3 lives." << endl;
-            cout << " • Join an extraordinary group of lifesavers." << endl;
-            cout << " • Donating regularly ensures that a safe and plentiful supply " << endl;
+            cout << " - One blood donation can save up to 3 lives." << endl;
+            cout << " - Join an extraordinary group of lifesavers." << endl;
+            cout << " - Donating regularly ensures that a safe and plentiful supply " << endl;
             cout << "   of blood is available wheneverand wherever it is needed. " << endl;
-            cout << " • Pay it forward. One day whanau, friends or even you may need it." << endl;
-            cout << " • Less than 3% of people in New Zealand are blood donors." << endl;
+            cout << " - Pay it forward. One day whanau, friends or even you may need it." << endl;
+            cout << " - Less than 3% of people in New Zealand are blood donors." << endl;
             break;
         }
         case 3: // Book Donation Appointment
@@ -571,9 +645,19 @@ void admin_menu()
 void line()
 {
     cout << endl;
-    for (int i = 0; i < 40; i++)
+    for (int i = 0; i < 97; i++)
     {
         cout << "-";
     }
     cout << endl;
+}
+
+void border_line()
+{
+    cout << endl << "+";
+    for (int i = 0; i < 95; i++)
+    {
+        cout << "-";
+    }
+    cout << "+" << endl;
 }
